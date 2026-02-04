6efcb327a3ed08aad46df975d46c8be46649a06d
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2026-02-04 13:14:17 -0600

    Se creo el proyecto Local DayTime se modifico la UI (compose) y se hicieron ajustes especificos para la platform desktop(jvm)

--- LocalTimeApp/composeApp/src/commonMain/kotlin/net/ivanvega/localtimeapp/App.kt
+++ LocalTimeApp/composeApp/src/commonMain/kotlin/net/ivanvega/localtimeapp/App.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,22 +19,53 @@ import org.jetbrains.compose.resources.painterResource
 
 import localtimeapp.composeapp.generated.resources.Res
 import localtimeapp.composeapp.generated.resources.compose_multiplatform
+import kotlin.time.Clock
+import kotlin.time.ExperimentalTime
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
         var timeAtLocation by remember { mutableStateOf("No location selected") }
+        var location by remember { mutableStateOf("Europe/Paris") }
 
+        val miOnClick = {
+            timeAtLocation = "13:30"
+        }
         Column(
             modifier = Modifier
                 .safeContentPadding()
                 .fillMaxSize(),
         ) {
             Text(timeAtLocation)
-            Button(onClick = { timeAtLocation = "13:30" }) {
+
+            val mi : (String) -> Unit = { p -> location = p   }
+            //val mi : (String) -> Unit = { location = it   }
+
+
+            //TextField(value = location, onValueChange = mi)
+
+            TextField(value = location, onValueChange = {location = it })
+
+            Button(onClick = miOnClick  ) {
                 Text("Show Time At Location")
             }
         }
     }
 }
+
+
+/*
+@OptIn(ExperimentalTime::class)
+fun currentTimeAt(location: String): String? {
+    fun LocalTime.formatted() = "$hour:$minute:$second"
+
+    return try {
+        val time = Clock.System.now()
+        val zone = TimeZone.of(location)
+        val localTime = time.toLocalDateTime(zone).time
+        "The time in $location is ${localTime.formatted()}"
+    } catch (ex: IllegalTimeZoneException) {
+        null
+    }
+}*/
