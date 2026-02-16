a45ff9bc4c656db81aee4be8ead8c64aad8b4c78
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2026-02-16 10:59:34 -0600

    Manejo de multiples ventanas y estado separado

--- KotlinMultiplatformDemoDesktop/composeApp/src/jvmMain/kotlin/net/ivanvega/kotlinmultiplatformdemodesktop/main.kt
+++ KotlinMultiplatformDemoDesktop/composeApp/src/jvmMain/kotlin/net/ivanvega/kotlinmultiplatformdemodesktop/main.kt
@@ -5,10 +5,13 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.DialogWindow
+import androidx.compose.ui.window.MenuBar
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import kotlinx.coroutines.delay
@@ -27,8 +30,67 @@ fun main() = application {
         }
     }*/
     //DosVentanatasCondicioopn()
-    DosVentanasYCoroutine(::exitApplication)
+    //DosVentanasYCoroutine(::exitApplication)
+
+    val applicationState = remember { MyApplicationState() }
+
+    for (window in applicationState.windows) {
+        key(window) {
+            MyWindow(window)
+        }
+    }
 }
+
+
+
+
+private class MyApplicationState {
+    val windows = mutableStateListOf<MyWindowState>()
+
+    init {
+        windows += MyWindowState("Initial window")
+    }
+
+    fun openNewWindow() {
+        windows += MyWindowState("Window ${windows.size}")
+    }
+
+    fun exit() {
+        windows.clear()
+    }
+
+    private fun MyWindowState(
+        title: String
+    ) = MyWindowState(
+        title,
+        openNewWindow = ::openNewWindow,
+        exit = ::exit,
+        windows::remove
+    )
+}
+
+private class MyWindowState(
+    val title: String,
+    val openNewWindow: () -> Unit,
+    val exit: () -> Unit,
+    private val close: (MyWindowState) -> Unit
+) {
+    fun close() = close(this)
+}
+
+
+@Composable
+private fun MyWindow(
+    state: MyWindowState
+) = Window(onCloseRequest = state::close, title = state.title) {
+    MenuBar {
+        Menu("File") {
+            Item("New window", onClick = state.openNewWindow)
+            Item("Exit", onClick = state.exit)
+        }
+    }
+}
+
 
 @Composable
 fun DosVentanasYCoroutine(perro: () -> Unit) {
