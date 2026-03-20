0af8af0eabdab8b325a8d4605e7d1cea22763d5a
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2026-03-20 16:26:00 -0600

    Se creo el ViewModel y DAO Se crearon las pantallas Se incluyo navegacuion Y se comenzo a introducir el tema de coroutines Y se enlazo las pantallas con el ViewModel Hasta este comentario, app ya inserta productos y lista productos.

---
+++ MiInventoryKMPComposeDesktop/composeApp/src/commonMain/kotlin/net/ivanvega/miinventorykmpcomposedesktop/ui/screens/AddItemScreen.kt
@@ -0,0 +1,55 @@
+package net.ivanvega.miinventorykmpcomposedesktop.ui.screens
+
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddItemScreen(viewModel: ItemViewModel, onItemAdded: () -> Unit, modifier: Modifier) {
+    var name by remember { mutableStateOf("") }
+    var price by remember { mutableStateOf("") }
+    var quantity by remember { mutableStateOf("") }
+
+
+        Column(
+            modifier = modifier,
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            OutlinedTextField(
+                value = name,
+                onValueChange = { name = it },
+                label = { Text("Name") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            OutlinedTextField(
+                value = price,
+                onValueChange = { price = it },
+                label = { Text("Price") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            OutlinedTextField(
+                value = quantity,
+                onValueChange = { quantity = it },
+                label = { Text("Quantity") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Button(
+                onClick = {
+                    viewModel.insertItem(
+                        name = name,
+                        price = price.toDoubleOrNull() ?: 0.0,
+                        quantity = quantity.toLongOrNull() ?: 0L
+                    )
+                    onItemAdded()
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Add")
+            }
+        }
+
+}
